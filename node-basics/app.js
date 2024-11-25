2eea96d5844e773836998de0d1453ab44593a74a
LucaHermann <lucab2c@outlook.fr>
2024-11-25 18:36:24 +0100

    add route.js, restructure app.js - lhermann

--- node-basics/app.js
+++ node-basics/app.js
@@ -1,45 +1,8 @@
-const http = require('http'); 
-const fs = require('fs'); 
+const http = require('http');
+const requestHandler = require('./routes');
 
 const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<div><form action="/messages" method="POST"><input type="text" name="username" /><button type="submit">Create User</button></form></div>');
-    return res.end();
-  } 
-
-  if (url === '/messages' && method === 'POST') {
-    const body = []
-    
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    });
-
-    return req.on('end', () => {
-      const bufferedBody = Buffer.concat(body).toString();
-      const message = bufferedBody.split('=')[1];
-      fs.writeFile('messages.txt', message, (err) => {
-        if (err) {
-          res.statusCode = 500;
-          console.log(err);
-          return res.end();
-        }
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>Node Basics</title></head>');
-  res.write('<body><h1>Node basics</h1></body>');
-  res.write('</html>');
-  res.end();
+  requestHandler.handler(req, res);
 });
 
 server.listen(3000); 
