3c3eb9c46a8169500bc91fd83b1f9212c02bafcd
LucaHermann <lucab2c@outlook.fr>
2024-12-29 12:29:39 +0100

    Refactor project structure and implement dynamic content routing
    - Updated package.json to point to the new app.js location.
    - Created new Express application in projects/dynamic-content-&-templates/app.js.
    - Added routing for admin and shop functionalities with corresponding views.
    - Implemented 404 error handling.
    - Introduced utility path module for cleaner path management.
    - Added package-lock.json and package.json for the new project structure.
    - Created HTML views for shop and add-product pages, along with a 404 error page.

--- projects/dynamic-content-&-templates/app.js
+++ projects/dynamic-content-&-templates/app.js
@@ -1,12 +1,12 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+
+const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const notFoundRoutes = require('./routes/404');
-
-const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
