8c6364a031bf14d8211d49f1c6ee0c326021cc20
LucaHermann <lucab2c@outlook.fr>
2024-12-30 16:39:13 +0100

    Implement MVC structure for product management in Express application. Created app.js, routes for admin and shop, and views for product handling, including add-product and product list. Added error handling for 404 responses and integrated EJS for templating. Updated package.json and package-lock.json for new dependencies. Cleaned up previous project structure by removing outdated files.

--- projects/node-basics/app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const requestHandler = require('./routes');
-
-const server = http.createServer((req, res) => {
-  requestHandler.handler(req, res);
-});
-
-server.listen(3000); 
