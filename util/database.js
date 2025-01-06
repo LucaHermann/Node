f57c1177c225a9b5a4c3306a27eadfadc1d96a33
LucaHermann <lucab2c@outlook.fr>
2025-01-06 17:36:52 +0100

    Refactor application to use Sequelize for database interactions and improve product management
    - Replaced direct MySQL queries with Sequelize ORM in the Product model for better abstraction and maintainability.
    - Updated the `postAddProduct` method in the admin controller to utilize Sequelize's `create` method for adding products.
    - Refactored the database connection setup in `util/database.js` to use Sequelize instead of MySQL2 pooling.
    - Enhanced the app initialization in `app.js` to synchronize the Sequelize models with the database before starting the server.
    - Removed outdated code related to manual database queries, streamlining the product management process.
    
    These changes enhance the application's data handling capabilities and improve the overall structure for future development.

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node_complete', 'root', 'nodecomplete', {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'node_complete',
-  password: 'nodecomplete',
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
