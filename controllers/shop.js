82b3a5f517a7467fe6e2e514963f67cdbf754f77
LucaHermann <lucab2c@outlook.fr>
2025-01-06 12:05:27 +0100

    Refactor product saving and improve error handling in admin and shop controllers
    - Updated the `postAddProduct` method in the admin controller to handle product saving with promise-based error handling.
    - Simplified the product fetching methods in the shop controller by removing unnecessary parameters from promise resolutions.
    - Enhanced the `save` method in the Product model to execute a database insert query for product creation.
    
    These changes improve the reliability of product management and enhance the overall error handling in the application.

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll().then(([rows, fieldData]) => {
+  Product.fetchAll().then(([rows]) => {
     res.render('shop/product-list', {
       prods: rows,
       pageTitle: 'All Products',
@@ -13,7 +13,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId).then(([rows, fieldData]) => {
+  Product.findById(prodId).then(([rows]) => {
     res.render('shop/product-detail', {
       product: rows[0],
       pageTitle: rows[0].title,
@@ -23,7 +23,7 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll().then(([rows, fieldData]) => {
+  Product.fetchAll().then(([rows]) => {
     res.render('shop/index', {
       prods: rows,
       pageTitle: 'Shop',
